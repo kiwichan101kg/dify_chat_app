4e5ac5f2b2ba08e61f48b2de646c9b7969a1c96f
kjnhk1001 <kjn.hk10001@gmail.com>
2024-06-01 13:49:31 +0900

    feat：マークダウン表示

--- src/screens/ChatScreen/index.tsx
+++ src/screens/ChatScreen/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ChatForm } from "./ChatForm";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 type Chat = { id: "string"; sender: "ai" | "user"; message: "string" };
 type ChatList = Chat[];
@@ -10,7 +12,7 @@ export const ChatScreen = async () => {
   return (
     <div className="flex flex-col h-screen bg-orange-50">
       <header className="bg-orange-400 text-white text-center py-4 shadow-md">
-        <h1 className="text-2xl font-bold">東京グルメbot</h1>
+        <h1 className="text-2l font-m">TOKYO GOURMET</h1>
       </header>
       <div className="flex-grow p-4 overflow-auto">
         <div className="flex flex-col space-y-4">
@@ -20,10 +22,12 @@ export const ChatScreen = async () => {
               className={`p-3 rounded-lg shadow-md max-w-md ${
                 chat.sender === "user"
                   ? "bg-orange-300 text-white self-end"
-                  : "bg-orange-200 text-gray-800 self-start flex items-center space-x-2"
+                  : "bg-orange-200 text-gray-800 self-start"
               }`}
             >
-              <span>{chat.message}</span>
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {chat.message}
+              </ReactMarkdown>
             </div>
           ))}
         </div>
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 export const HomeScreen = () => {
-  return <div>home</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-orange-50">
+      <div className="mb-10 text-center">
+        <h1 className="text-2xl  text-orange-400"> TOKYO GOURMET</h1>
+      </div>
+      <div className="flex flex-col space-y-6 mt-10">
+        <Link href="/chat">
+          <span className="px-8 py-4 bg-orange-300 text-white text-lg rounded-lg shadow-md hover:bg-orange-400 transition duration-300 cursor-pointer text-center w-64">
+            START
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 };
